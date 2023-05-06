45c701498e1ca641cb2cd7e530a611a30063c233
Yevhenii Sivukhin <fleffydesign@gmail.com>
2023-05-06 15:42:27 +0300

    added some hover animations

--- src/components/Header/Buttons.jsx
+++ src/components/Header/Buttons.jsx
@@ -3,13 +3,13 @@ import { BiPlus, BiTrash, BiPencil } from 'react-icons/bi'
 export default function Buttons() {
 	return (
 		<div className='flex gap-3'>
-			<button className='rounded-lg border-[1px] p-3'>
+			<button className='rounded-lg border-[1px] p-3 transition-all hover:bg-[#4338ca] hover:text-white'>
 				<BiPlus />
 			</button>
-			<button className='rounded-lg border-[1px] p-3'>
+			<button className='rounded-lg border-[1px] p-3 transition-all hover:bg-[#4338ca] hover:text-white'>
 				<BiTrash />
 			</button>
-			<button className='rounded-lg border-[1px] p-3'>
+			<button className='rounded-lg border-[1px] p-3 transition-all hover:bg-[#4338ca] hover:text-white'>
 				<BiPencil />
 			</button>
 		</div>
--- src/components/Workspace/Workspace.jsx
+++ src/components/Workspace/Workspace.jsx
@@ -1,7 +1,7 @@
 export default function Workspace() {
 	return (
 		<aside className='h-screen w-full transition-all'>
-			<div className='flex h-full flex-col gap-3 overflow-y-auto rounded-t-2xl bg-gray-200 p-2 dark:bg-[#4338ca]/60 md:rounded-l-2xl md:rounded-tr-none'>
+			<div className='flex h-full flex-col gap-2 overflow-y-auto rounded-t-2xl bg-gray-200 p-2 dark:bg-[#4338ca]/60 md:rounded-l-2xl md:rounded-tr-none'>
 				<button
 					title='edit title'
 					className='w-full overflow-hidden rounded-t-2xl bg-white p-4  text-lg font-bold text-slate-800 dark:bg-[#1c1c1c] dark:text-slate-50'
@@ -9,7 +9,7 @@ export default function Workspace() {
 					Note Title
 				</button>
 				<textarea
-					className='h-full w-full rounded-b-2xl p-8 font-mono text-2xl text-slate-800 outline-none dark:bg-[#1c1c1c] dark:text-slate-50'
+					className='h-full w-full resize-none rounded-b-2xl p-8 font-mono text-2xl text-slate-800 outline-none dark:bg-[#1c1c1c] dark:text-slate-50'
 					type='text'
 				/>
 			</div>
